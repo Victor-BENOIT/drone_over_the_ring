0c878aa9219248cb7b188a80db1acd2644983d16
Victor Benoit <victor.benoit@reseau.eseo.fr>
2024-10-25 11:03:00 +0200

    Logging deplacemeent

--- drone_controller/movement.py
+++ drone_controller/movement.py
@@ -8,18 +8,24 @@ class Movement:
 
     def move_forward(self, distance = DRONE_DIST):
         self.tello.move_forward(distance)
+        self.logging.add_movement('forward', distance)
 
     def move_backward(self, distance = DRONE_DIST):
         self.tello.move_back(distance)
+        self.logging.add_movement('backward', distance)
 
     def move_left(self, distance = DRONE_DIST):
         self.tello.move_left(distance)
+        self.logging.add_movement('left', distance)
 
     def move_right(self, distance = DRONE_DIST):
         self.tello.move_right(distance)
+        self.logging.add_movement('right', distance)
 
     def move_up(self, distance = DRONE_DIST):
         self.tello.move_up(distance)
+        self.logging.add_movement('up', distance)
 
     def move_down(self, distance = DRONE_DIST):
         self.tello.move_down(distance)
+        self.logging.add_movement('down', distance)
